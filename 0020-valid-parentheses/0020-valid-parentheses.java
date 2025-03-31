73a87ab99c9b7befa5409fd7f25e3f2aa88cf7fb
Rishika11 <140777676+SugaliRishika@users.noreply.github.com>
2025-04-01 00:20:26 +0530

    Time: 2 ms (97.57%), Space: 42 MB (20.32%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,21 +1,36 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        Map<Character, Character> mapping = new HashMap<>();
-        mapping.put(')', '(');
-        mapping.put('}', '{');
-        mapping.put(']', '[');
+        // Stack<Character> stack = new Stack<>();
+        // Map<Character, Character> mapping = new HashMap<>();
+        // mapping.put(')', '(');
+        // mapping.put('}', '{');
+        // mapping.put(']', '[');
 
-        for (char c : s.toCharArray()) {
-            if (mapping.containsValue(c)) {
-                stack.push(c);
-            } else if (mapping.containsKey(c)) {
-                if (stack.isEmpty() || mapping.get(c) != stack.pop()) {
+        // for (char c : s.toCharArray()) {
+        //     if (mapping.containsValue(c)) {
+        //         stack.push(c);
+        //     } else if (mapping.containsKey(c)) {
+        //         if (stack.isEmpty() || mapping.get(c) != stack.pop()) {
+        //             return false;
+        //         }
+        //     }
+        // }
+
+        // return stack.isEmpty();  
+        Stack<Character> st=new Stack<>();
+        for(char it:s.toCharArray()){
+            if(it=='('||it=='{'||it=='['){
+                st.push(it);
+            }else{
+                if(st.isEmpty()) return false;
+                char ch=st.pop();
+                if((it==')' && ch=='(') || (it== '}' && ch=='{') || (it==']' && ch=='[')){
+                    continue;
+                }else{
                     return false;
                 }
             }
         }
-
-        return stack.isEmpty();        
+        return st.isEmpty();
     }
 }
