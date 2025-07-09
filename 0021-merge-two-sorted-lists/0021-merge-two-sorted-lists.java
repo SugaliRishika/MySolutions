d577c92ee0d0386a7447211f9a503109d47d0cb1
Rishika11 <140777676+SugaliRishika@users.noreply.github.com>
2025-07-09 23:05:45 +0530

    Time: 0 ms (100%), Space: 42.6 MB (59.45%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -7,34 +7,38 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
- */
+*/
+
 // class Solution {
 //     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-//         ListNode dummy = new ListNode();
-//         ListNode cur = dummy;
-
-//         while (list1 != null && list2 != null) {
-//             if (list1.val > list2.val) {
-//                 cur.next = list2;
-//                 list2 = list2.next;
-//             } else {
-//                 cur.next = list1;
-//                 list1 = list1.next;
+//         ListNode dummy=new ListNode();
+//         ListNode curr=dummy;
+//         while(list1!=null && list2!=null){
+//             if(list1.val>list2.val){
+//                 curr.next=list2;
+//                 list2=list2.next;
+//             }else{
+//                 curr.next=list1;
+//                 list1=list1.next;
 //             }
-//             cur = cur.next;
+//             curr=curr.next;
 //         }
-
-//         cur.next = (list1 != null) ? list1 : list2;
-
-//         return dummy.next;        
+//         if(list1!=null){
+//             curr.next=list1;
+//         }else{
+//             curr.next=list2;
+//         } 
+//         return dummy.next;   
 //     }
 // }
+
 
 
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode dummy=new ListNode();
         ListNode curr=dummy;
+
         while(list1!=null && list2!=null){
             if(list1.val>list2.val){
                 curr.next=list2;
@@ -43,13 +47,15 @@ class Solution {
                 curr.next=list1;
                 list1=list1.next;
             }
+
             curr=curr.next;
         }
+
         if(list1!=null){
             curr.next=list1;
         }else{
             curr.next=list2;
-        } 
-        return dummy.next;   
+        }
+        return dummy.next;
     }
 }
