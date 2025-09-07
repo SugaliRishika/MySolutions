d19c9bcc6cc71fd94b7d52c94670dd92c53a6de1
Rishika11 <140777676+SugaliRishika@users.noreply.github.com>
2025-09-07 10:39:49 +0530

    Time: 4 ms (31.17%), Space: 46 MB (73.35%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -3,27 +3,50 @@ import java.util.Arrays;
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         
-        int n = nums1.length;
-        int m = nums2.length;
+        // int n = nums1.length;
+        // int m = nums2.length;
 
-        int[] merged = new int[n + m];
-        int k = 0;
-        for (int i = 0; i < n; i++) {
-            merged[k++] = nums1[i];
+        // int[] merged = new int[n + m];
+        // int k = 0;
+        // for (int i = 0; i < n; i++) {
+        //     merged[k++] = nums1[i];
+        // }
+        // for (int i = 0; i < m; i++) {
+        //     merged[k++] = nums2[i];
+        // }
+        // Arrays.sort(merged);
+        // int total = merged.length;
+
+        // if (total % 2 == 1) {
+        //     return (double) merged[total / 2];
+        // } else {
+        //     int middle1 = merged[total / 2 - 1];
+        //     int middle2 = merged[total / 2];
+        //     return ((double) middle1 + (double) middle2) / 2.0;
+        // }
+
+        int n=nums1.length;
+        int m=nums2.length;
+
+        int[] merged=new int[n+m];
+        int k=0;
+        for(int i=0;i<n;i++){
+            merged[k++]=nums1[i];
         }
-        for (int i = 0; i < m; i++) {
-            merged[k++] = nums2[i];
+
+        for(int i=0;i<m;i++){
+            merged[k++]=nums2[i];
         }
 
         Arrays.sort(merged);
-        int total = merged.length;
+        int len=merged.length;
 
-        if (total % 2 == 1) {
-            return (double) merged[total / 2];
-        } else {
-            int middle1 = merged[total / 2 - 1];
-            int middle2 = merged[total / 2];
-            return ((double) middle1 + (double) middle2) / 2.0;
+        if(len%2==1){
+            return (double)merged[len/2];
+        }else{
+            int mid1=merged[len/2-1];
+            int mid2=merged[len/2];
+            return ((double) mid1+(double) mid2)/2.0;
         }
     }
 }
