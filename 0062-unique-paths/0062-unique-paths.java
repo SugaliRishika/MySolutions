24dcf0fee363e41a3b634400f3bed0e97c954d28
Rishika11 <140777676+SugaliRishika@users.noreply.github.com>
2025-09-13 22:44:30 +0530

    Time: 0 ms (100%), Space: 40.7 MB (25.9%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,24 +1,17 @@
 // class Solution {
-//     public int func(int i,int j,int[][] dp){
-//         if(i==0 && j==0){
-//             return 1;
-//         }
-//         if(i<0 && j<0){
-//             return 0;
-//         }
-//         if(dp[i][j]!=-1){
-//             return dp[i][j];
-//         }
-//         int up=func(i-1,j,dp);
-//         int left=func(i,j-1,dp);
-//         return dp[i][j]=up+left;
-//     }
 //     public int uniquePaths(int m, int n) {
-//         int[][] dp=new int[n][4];
-//         for(int[] num:dp){
-//             Arrays.fill(num,-1);
+//         int[][] dp=new int[m][n];
+//         dp[0][0]=1;
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(i==0 || j==0){
+//                     dp[i][j]=1;
+//                 }else{
+//                 dp[i][j]=dp[i-1][j]+dp[i][j-1];
+//                 }
+//             }
 //         }
-//         return func(m-1,n-1,dp);
+//         return dp[m-1][n-1];
 //     }
 // }
 
@@ -26,14 +19,13 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp=new int[m][n];
-        dp[0][0]=1;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 || j==0){
-                    dp[i][j]=1;
-                }else{
+
+        for(int i=0;i<m;i++) dp[i][0]=1;
+        for(int j=0;j<n;j++) dp[0][j]=1;
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
                 dp[i][j]=dp[i-1][j]+dp[i][j-1];
-                }
             }
         }
         return dp[m-1][n-1];
