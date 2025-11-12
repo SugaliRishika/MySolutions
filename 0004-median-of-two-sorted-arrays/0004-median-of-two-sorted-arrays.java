1de9ecc29827de9471f088e32b6c51af61314203
Sugali Rishika <140777676+SugaliRishika@users.noreply.github.com>
2025-11-12 17:40:47 +0530

    Time: 1 ms (100%), Space: 48.9 MB (6.48%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,52 +1,33 @@
-import java.util.Arrays;
-
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        
-        // int n = nums1.length;
-        // int m = nums2.length;
-
-        // int[] merged = new int[n + m];
-        // int k = 0;
-        // for (int i = 0; i < n; i++) {
-        //     merged[k++] = nums1[i];
-        // }
-        // for (int i = 0; i < m; i++) {
-        //     merged[k++] = nums2[i];
-        // }
-        // Arrays.sort(merged);
-        // int total = merged.length;
-
-        // if (total % 2 == 1) {
-        //     return (double) merged[total / 2];
-        // } else {
-        //     int middle1 = merged[total / 2 - 1];
-        //     int middle2 = merged[total / 2];
-        //     return ((double) middle1 + (double) middle2) / 2.0;
-        // }
-
-        int n=nums1.length;
-        int m=nums2.length;
-
-        int[] merged=new int[n+m];
-        int k=0;
-        for(int i=0;i<n;i++){
-            merged[k++]=nums1[i];
+        if(nums1.length>nums2.length){
+            return findMedianSortedArrays(nums2,nums1);
         }
 
-        for(int i=0;i<m;i++){
-            merged[k++]=nums2[i];
-        }
+        int m=nums1.length;
+        int n=nums2.length;
+        int low=0,high=m;
 
-        Arrays.sort(merged);
-        int len=merged.length;
+        while(low<=high){
+            int x=(low+high)/2;
+            int y=(m+n+1)/2-x;
 
-        if(len%2==1){
-            return (double)merged[len/2];
-        }else{
-            int mid1=merged[len/2-1];
-            int mid2=merged[len/2];
-            return ((double) mid1+(double) mid2)/2.0;
+            int leftmax=(x==0)?Integer.MIN_VALUE:nums1[x-1];
+            int rightmin=(x==m)?Integer.MAX_VALUE:nums1[x];
+            int maxleftY=(y==0)?Integer.MIN_VALUE:nums2[y-1];
+            int minrightY=(y==n)?Integer.MAX_VALUE:nums2[y];
+
+            if(leftmax<=minrightY && maxleftY<=rightmin){
+                if((m+n)%2==0)
+                    return (Math.max(leftmax,maxleftY)+Math.min(rightmin,minrightY))/2.0;
+                else
+                    return Math.max(leftmax,maxleftY);
+            }else if(leftmax>minrightY){
+                high=x-1;
+            }else{
+                low=x+1;
+            }
         }
+        return 0.0;
     }
 }
